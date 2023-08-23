3c3f6d123c1a3f5619186eefa98fd74919cb2574
MrAmanJoshi <113497951+MrAmanJoshi@users.noreply.github.com>
2023-08-23 07:09:51 +0000

    Remove repeated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import CurrentDay from "./CurrentDay"
-import PriviousDay from "./PriviousDay"
+import DayTask from "./DayTask";
+import {priviousDayData} from "./Task_Data";
+import { CurrentDayData } from "./Task_Data";
 
+//Now I have deleted the currentDay and previousDay component, now I have created new component named Daytask and Profile and my DayTask component is reusable. By this I have reduced the repeating code.
 
 function App() {
+  
+const currentDate = {
+  date: 23, 
+  day: "WED"
+}
+const PriviousDate = {
+  date: 22, 
+  day: "TUE"
+}
+  
   return (
 
     <div className="border rounded-lg m-1 max-w-[1440]  bg-slate-50 relative ">
       <div className="w-[0.5px]  h-[968px] sm:h-[870px] mt-[87px] sm:mt-[149px] sm:ml-[237px] ml-5 bg-gray-300 absolute  z-10"></div>
       <div className=" h-[0.5px] sm:w-[620px]  mt-[87px] sm:mt-[627px] sm:ml-[237.7px] ml-5 bg-gray-300 absolute z-10"></div>
      
-      {
-        //I have divided it into two components to make this page
-      }
         
       <div className="mt-4">
-        
-        
-        <CurrentDay />
-        <div className="my-6">
-          <PriviousDay />
-        </div>
-      </div>
-
+             
+          <DayTask 
+            data={CurrentDayData}
+            date={currentDate}
+            />
+                  
+          <DayTask 
+            data={priviousDayData}
+            date={PriviousDate}
+           />
+               
+</div>
     </div>
 
   );
---
+++ src/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Profile = () => (
+  
+  <div className="flex ml-1 sm:ml-0 z-20">
+                    <img src="https://i.postimg.cc/DZhWJSyX/Photo.jpg" className="w-8 h-8 rounded-full bg-red-300 mr-3" />
+                    <p className="text-slate-400 mt-2">Sheila O’Riley</p>
+                  </div>
+    
+)
+
+export default Profile
